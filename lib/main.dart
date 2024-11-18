53ab4f6ec6bcb74458a5a2fa92e6f62cdd7db760
Armaan Singh <arma0017@algonquinlive.com>
2024-11-18 10:25:53 -0500

    Initail commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:floor/floor.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+import 'dart:async';
 import 'database.dart';
 import 'car_list_page.dart';
 
@@ -11,17 +14,39 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   final AppDatabase database;
-
   MyApp(this.database);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Car Management',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: CarListPage(database), // Pass AppDatabase here
+      home: MainPage(database),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  final AppDatabase database;
+  MainPage(this.database);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CarListPage(database.carDao)),
+          ),
+          child: Text('View Car List'),
+        ),
+      ),
     );
   }
 }
